173a36cea76a0659f93025a57f0485eaa37cd87a
ThommyJP <twjp@criptext.com>
2022-08-29 14:20:30 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	console.log(props);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bg-dark display-1 d-flex justify-content-center">
+			<div className="text-white">🕐</div>
+			<div className="text-white">{props.cienmil}</div>
+			<div className="text-white">{props.diezmil}</div>
+			<div className="text-white">{props.mil}</div>
+			<div className="text-white">{props.cien}</div>
+			<div className="text-white">{props.diez}</div>
+			<div className="text-white">{props.uno}</div>
 		</div>
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,65 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "../styles/index.scss";
 
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let unsegundo = 0;
+let diezsegundos = 0;
+let ciensegundos = 0;
+let milsegundos = 0;
+let diezmilsegundos = 0;
+let cienmilsegundos = 0;
+
+setInterval(() => {
+    unsegundo++;
+      if (unsegundo > 9) { 
+        unsegundo = 0
+
+        diezsegundos++
+        if (diezsegundos > 5) {
+            diezsegundos = 0
+
+            ciensegundos++
+            if (ciensegundos > 9) {
+                ciensegundos = 0
+
+                milsegundos++
+                if (milsegundos > 5) {
+                    milsegundos = 0
+
+                    diezmilsegundos++
+                    if (diezmilsegundos > 9) {
+                        diezmilsegundos = 0
+
+                        cienmilsegundos++
+                        if (cienmilsegundos > 9) {
+                            cienmilsegundos= 0
+                        }
+                        
+                    }
+                    
+                }
+                
+            }            
+        }
+    } 
+       
+
+ //render your react application
+    ReactDOM.render(
+        <div className="py-5 background">
+	        <Home
+            uno={unsegundo}
+            diez={diezsegundos}
+            cien={ciensegundos}
+            mil={milsegundos}
+            diezmil={diezmilsegundos}
+            cienmil={cienmilsegundos}/>
+        </div>, 
+
+    document.querySelector("#app")
+    );
+}	, 1000)
